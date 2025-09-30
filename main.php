48f2f35ee94fd72ad4d29bc28d793f3c2d2dffcf
mona-suke <smthrme@gmail.com>
2025-09-30 15:13:55 +0900

    Update main.php

--- main.php
+++ main.php
@@ -364,8 +364,8 @@
 		<div class="result_box"  id ="myTable">
 			<table class ="species_name" id="trg_name"></table>
 			<table hidden>
-				<tr><td>【著作権について】産業新聞社の許可なく配信データを転送、複製、販売などの行為をすることはご遠慮願います。</td></tr>
-				<tr><td>産業新聞社調べ、調べ日ベース</td></tr>
+				<tr><td>【著作権について】許可なく配信データを転送、複製、販売などの行為をすることはご遠慮願います。</td></tr>
+				<tr><td>調べ日ベース</td></tr>
 			</table>
 			
 			<div class="result_table"></div>
@@ -380,7 +380,6 @@
 		e.preventDefault(); // ページリロードを止める
 		const species_id = $('#species_id').val();
 		
-		// ★ここに品種リスト取得いれる
 		const trg_species = document.querySelectorAll('#selected_tags span');
 		const species_lists = Array.from(trg_species).map((x) => x.dataset.id);
 
@@ -590,3 +589,4 @@
 
 </body>
 </html>
+
